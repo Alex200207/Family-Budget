0f61ca427cc102de8b4dd343a06c034656ba7157
soy eddy <soyeddy.email.com>
2024-11-30 13:37:00 -0600

    agregando atajos

--- src/components/GraficaPresupuesto.tsx
+++ src/components/GraficaPresupuesto.tsx
@@ -1,16 +1,11 @@
+import { Link } from "react-router-dom";
 import useBudget from "../hook/useBudget";
-
-export interface Presupuesto {
-  id: number;
-  categoria: string;
-  fecha_inicio?: Date;
-  fecha_fin?: Date;
-  limite: number;
-  usuario_id: number;
-}
+import useExpenses from "../hook/useExpenses";
+import { Gasto, Presupuesto } from "../types";
 
 export default function GraficaPresupuesto() {
   const { budget } = useBudget();
+  const { expense } = useExpenses();
 
   const categoryColors: Record<string, string> = {
     Housing: "bg-blue-500",
@@ -20,28 +15,34 @@ export default function GraficaPresupuesto() {
     Healthcare: "bg-red-500",
   };
 
-  // Limitar a los primeros tres presupuestos
   const displayedBudgets = budget.slice(0, 3);
 
   return (
     <div className="bg-white rounded-xl shadow-sm p-6">
-      <h2 className="text-xl font-semibold mb-6">Presupuesto</h2>
+      <div className="flex justify-between items-center mb-6">
+        <h2 className="text-xl font-semibold">Presupuesto</h2>
+        <button className="text-indigo-600 hover:text-indigo-700 font-medium">
+          <Link to="/presupuestos">Ver Todos</Link>
+        </button>
+      </div>
+
       {displayedBudgets && displayedBudgets.length > 0 ? (
         <div className="space-y-4">
           {displayedBudgets.map((b: Presupuesto) => {
-            const color = categoryColors[b.categoria] || "bg-gray-500";
+            const totalSpent = expense
+              .filter((e: Gasto) => e.presupuesto_id === b.id)
+              .reduce((acc, e) => acc + e.monto, 0);
 
-            const percentageUsed = Math.min(
-              100,
-              parseFloat((Math.random() * 100).toFixed(0))
-            );
+            const percentageUsed = Math.min(100, (totalSpent / b.limite) * 100);
+
+            const color = categoryColors[b.categoria] || "bg-gray-500";
 
             return (
               <div key={b.id} className="space-y-2">
                 <div className="flex justify-between">
                   <span className="text-sm font-medium">{b.categoria}</span>
                   <span className="text-sm text-gray-500">
-                    {percentageUsed}%
+                    {percentageUsed.toFixed(2)}%
                   </span>
                 </div>
                 <div className="h-2 bg-gray-200 rounded-full overflow-hidden">
